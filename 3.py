e49cbda0c9e3325c34cd97c937984497e6f59009
builtQ <45253856+builtQ@users.noreply.github.com>
2018-12-04 19:10:45 +0800

    Update 3.py

--- 3.py
+++ 3.py
@@ -15,4 +15,17 @@ for root,files,dirs in os.walk(root_path):
 	 	#print(d)
 	 	dirs_name = os.path.splitext(d)[0]
 	 	print("\t"*(indent_level+1),dirs_name)
+		
+		
+		
+import os
+root_path=os.getcwd()
+offset = len(root_path.split("\\"))
+
+for root,dirs,files in os.walk(root_path):
+	current_dir = root.split("\\")
+	indent=len(current_dir)-offset
+	print("\t"*indent,"\\"+current_dir[-1])
+	for f in files:
+		print("\t"*(indent+1),f)
   
