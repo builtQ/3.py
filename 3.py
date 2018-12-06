cf8389fe9a27d0df80f8c6704fa7dc1fde2b74d4
builtQ <45253856+builtQ@users.noreply.github.com>
2018-12-06 15:12:00 +0800

    Update 3.py

--- 3.py
+++ 3.py
@@ -30,3 +30,15 @@ for root,dirs,files in os.walk(root_path):
 	for f in files:
 		print("\t"*(indent+1),f)
   
+
+import os
+line_count,blank_count=0,0
+with open("a.txt") as fp:
+	while True:
+		line=fp.readline()
+		if not line:
+			break
+		line_count+=1
+		if  len(line.strip())==0:
+			blank_count+=1
+print(line_count,"Lines(",blank_count,"blanks)")#a文本中的行数
